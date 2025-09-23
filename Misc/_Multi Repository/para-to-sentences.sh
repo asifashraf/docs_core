5ad24fd96f2c9c08405e55c4be71a74de07cfd24
Phil <phillip.gideon@dcisolution.com>
2025-09-23 02:16:35 -0400

    Initial commit

--- Misc/_Multi Repository/para-to-sentences.sh
+++ Misc/_Multi Repository/para-to-sentences.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-FILE_PATH="/Users/rcarrion/data/r-docs_readings/CloudNative/CN Devops orielly/005-Open-Source Tools for Cloud-Native DevOps.md"
+FILE_PATH="/Users/rcarrion/data/r-docs_readings/Networking/Networking for dummies all in one/b1 getting started with nw/ch003/003-Switches, Routers, and VLANs.md"
 
 # Use sed to replace ". " with "\r\n\r\n" in place (macOS-compatible)
-sed -i '' 's/\. /\r\n~\r\n~\r\n/g' "$FILE_PATH"
+sed -i '' 's/\. /. ________|________ /g' "$FILE_PATH"
+
+sed -i '' 's/ / /g' "$FILE_PATH"
+
+sed -i '' 's|!\[Bullet\](https://learning\.oreilly\.com/api/v2/epubs/urn:orm:book:9781394278381/files/images/check\.png)|Bullet :|g' "$FILE_PATH"
 
 echo "Replacement completed in $FILE_PATH"
+
